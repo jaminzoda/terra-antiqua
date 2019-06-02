4eb5bfe60960f418ac5d4407471ab6b8db60219e
jon <jaminov@protonmail.com>
2019-06-02 18:42:17 +0200

    Raster-smoothing

---
+++ test/test-code.py
@@ -0,0 +1,44 @@
+from osgeo import gdal
+from matplotlib import pyplot as plt
+import numpy as np
+path="/home/jon/PycharmProjects/databundle/data-80Ma/Results/paleo_dem_copy.tif"
+
+
+
+# file=gdal.Open(path)
+# array=file.GetRasterBand(1).ReadAsArray()
+# sarray=array[1000:1300,1100:1500]
+#
+# sarray[np.isnan(sarray)]=0
+sarray=np.random.random((10,10))
+#print(sarray)
+
+#for i,j in np.ndindex(sarray.shape):
+#     subset=sarray[i-3:i+3,j-3:j+3]
+#     print([i-3:i+3,j-3:j+3])
+#     sarray[i,j]=np.mean(subset)
+
+#plt.imshow(sarray)
+#plt.show()
+factor=3
+
+cols=sarray.shape[1]
+rows=sarray.shape[0]
+out_array=np.array(sarray)
+#out_array[:]=sarray[:]
+import time
+
+for i in range(rows):
+    for j in range(cols):
+        # Define smoothing mask; periodic boundary along date line
+
+        x_vector = np.mod((np.arange((i - factor), (i + factor + 1))), (cols))
+        #x_vector = x_vector.reshape(1, len(x_vector))
+        y_vector = np.arange(np.maximum(0, j - factor), (np.minimum((rows - 1), j + factor) + 1), 1)
+        y_vector = y_vector.reshape(len(y_vector), 1)
+
+        out_array[i, j] = np.mean(sarray[y_vector, x_vector])
+        print(sarray[y_vector,x_vector])
+        #print(y_vector,x_vector)
+
+#print(out_array)
--- topotools.py
+++ topotools.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from osgeo import gdal, gdalconst
+from qgis.core import QgsRasterLayer
 
 import os
 from .topo_modifier_dialog import TopoModifierDialog  as td
@@ -9,32 +10,13 @@ from .topo_modifier_dialog import TopoModifierDialog  as td
 
 
 
-class TopoTools:
+class RasterTools(QgsRasterLayer):
 	def __init__(self):
-		pass
+		super.__init__(self)
 
-	def mod_min_max(in_array, fmin:int, fmax:int):
-		"""
-		Modifies the elevation/bathimetry
-		values based on the current and provided
-		minimum and maximum values.
-		This is basically flattening and roughening.
-		:param in_array: input numpy array for modification.
-		:param fmin: final minimum value of elevation/bathymetry.
-		:param fmax: final maximum value of elevation/bathymetry.
-		:return: modified array of eleveation/bathymetry values.
-		"""
 
-		#Define the initial minimum and maximum values of the array
-		#imin = in_array.min()
-		imax = in_array[np.isfinite(in_array)].max()
-		out_array=in_array
 
-		ratio=(imax-fmin)/(fmax-fmin)
-		out_array[out_array>=fmin]=fmin+(in_array[in_array>=fmin]-fmin)/ratio
-		return out_array
-
-	def fill_no_data(in_layer, no_data_value=None):
+	def fill_no_data(self, no_data_value=None):
 		"""
 		Fils the missing data by interpolating from edges.
 		:param in_layer: QgsRasterLayer
@@ -42,7 +24,7 @@ class TopoTools:
 		:return: Boolean
 		"""
 		#Get the input raster dataset
-		rlayer=in_layer
+		rlayer=self
 		raster_ds = gdal.Open(rlayer.dataProvider().dataSourceUri(), gdalconst.GA_Update)
 		in_band = raster_ds.GetRasterBand(1)
 		in_array = in_band.ReadAsArray()
@@ -83,14 +65,14 @@ class TopoTools:
 		out_raster = None
 		return result
 
-	def raster_smoothing(in_layer, factor):
+	def raster_smoothing(self, factor):
 		"""
 		Smoothes values of pixels in a raster  by averaging  values around them
 		:param in_layer: input raster layer for smoothing
 		:return:Boolean
 		"""
 
-		rlayer = in_layer
+		rlayer = self
 		raster_ds = gdal.Open(rlayer.dataProvider().dataSourceUri(), gdalconst.GA_Update)
 		in_band = raster_ds.GetRasterBand(1)
 		in_array = in_band.ReadAsArray()
@@ -99,19 +81,43 @@ class TopoTools:
 
 		out_array=np.zeros(in_array.shape)
 
-		import time
-		start=time.time()
-		for i in range(rows):
-			for j in range(cols):
+
+		for i in range(rows-1):
+			for j in range(cols-1):
 				# Define smoothing mask; periodic boundary along date line
-				x_vector = np.mod((np.arange((i - factor), (i + factor + 1))), (cols))
+				x_vector = np.mod((np.arange((j - factor), (j + factor + 1))), (cols-1))
 				x_vector = x_vector.reshape(1, len(x_vector))
-				y_vector = np.arange(np.maximum(0, j - factor), (np.minimum((rows - 1), j + factor) + 1), 1)
+				y_vector = np.arange(np.maximum(0, i - factor), (np.minimum((rows-1), i + factor) + 1), 1)
 				y_vector = y_vector.reshape(len(y_vector), 1)
 				out_array[i, j] = np.mean(in_array[y_vector, x_vector])
-		end=time.time()
-		print(end-start)
+
+
 
 		in_band.WriteArray(out_array)
 		in_band.FlushCache()
 		return True
+
+
+class ArrayTools(np.ndarray):
+	def __init__(self):
+		super.__init__(self)
+	def mod_min_max(self, fmin:int, fmax:int):
+		"""
+		Modifies the elevation/bathimetry
+		values based on the current and provided
+		minimum and maximum values.
+		This is basically flattening and roughening.
+		:param in_array: input numpy array for modification.
+		:param fmin: final minimum value of elevation/bathymetry.
+		:param fmax: final maximum value of elevation/bathymetry.
+		:return: modified array of eleveation/bathymetry values.
+		"""
+		in_array=self
+		#Define the initial minimum and maximum values of the array
+		#imin = in_array.min()
+		imax = in_array[np.isfinite(in_array)].max()
+		out_array=in_array
+
+		ratio=(imax-fmin)/(fmax-fmin)
+		out_array[out_array>=fmin]=fmin+(in_array[in_array>=fmin]-fmin)/ratio
+		return out_array
